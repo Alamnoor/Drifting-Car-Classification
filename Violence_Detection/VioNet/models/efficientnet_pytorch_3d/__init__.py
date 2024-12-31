0ffc0a78b64ddd8644df07c85c16e593a52c6073
Alamnoor <34956094+Alamnoor@users.noreply.github.com>
2024-12-31 11:31:09 +0000

    Add files via upload

---
+++ Violence_Detection/VioNet/models/efficientnet_pytorch_3d/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.1"
+from .efficientnet3d import EfficientNet3D
+from .utils import (
+    GlobalParams,
+    BlockArgs,
+    BlockDecoder,
+    efficientnet3d,
+    get_model_params,
+)
+
